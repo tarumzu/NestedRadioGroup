e1db4301ed1511d84e2687ad500aefb8d68aa9c7
tarumzu <wwww.0909mizu@gmail.com>
2020-10-04 00:41:52 +0900

    add NestedRadioGroup

---
+++ nestedradiogroup/src/main/java/com/tarumzu/nestedradiogroup/NestedRadioGroup.kt
@@ -0,0 +1,58 @@
+package com.tarumzu.nestedradiogroup
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CompoundButton
+import android.widget.RadioButton
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class NestedRadioGroup(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+    private val radioButtons = mutableMapOf<Int, RadioButton>()
+    private var checkedId = -1
+    private var onCheckedChangeListener: OnCheckedChangeListener? = null
+    private var onClickListener: OnClickListener? = null
+
+    override fun addView(child: View, index: Int, params: ViewGroup.LayoutParams?) {
+        super.addView(child, index, params)
+        findRadioButtons(child)
+    }
+
+    private fun findRadioButtons(child: View) {
+        if (child is RadioButton) {
+            child.setOnCheckedChangeListener { buttonView: CompoundButton, isChecked: Boolean ->
+                if (isChecked.not()) return@setOnCheckedChangeListener
+
+                if (checkedId != -1) radioButtons[checkedId]?.isChecked = false
+                radioButtons[buttonView.id]?.isChecked = true
+                checkedId = buttonView.id
+                onCheckedChangeListener?.onCheckedChanged(radioButtons, child.id)
+            }
+            child.setOnClickListener {
+                onClickListener?.onClick(it)
+            }
+            radioButtons.put(child.id, child)
+        } else if (child is ViewGroup) {
+            for (i in 0 until child.childCount) {
+                findRadioButtons(child.getChildAt(i))
+            }
+        }
+    }
+
+    fun setOnCheckedChangeListener(onCheckedChangeListener: OnCheckedChangeListener?) {
+        this.onCheckedChangeListener = onCheckedChangeListener
+    }
+
+    fun setOnClickListener(onClickListener: OnClickListener?) {
+        this.onClickListener = onClickListener
+    }
+
+    interface OnCheckedChangeListener {
+        fun onCheckedChanged(var1: MutableMap<Int, RadioButton>, var2: Int)
+    }
+
+    interface OnClickListener {
+        fun onClick(var1: View?)
+    }
+}
